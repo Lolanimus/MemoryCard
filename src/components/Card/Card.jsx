4e66771101d3876af3bea3a566deb78b017129c4
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-06-27 11:54:08 +0300

    Fetching now works. But need to make it fetch different gif each call

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,18 +2,34 @@ import { useEffect, useState } from "react";
 import style from "./Card.module.scss";
 
 export default function Card() {
-    const [img, setImg] = useState("");
+    const [img, setImg] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const fetchingImage = async () => {
+        try {
+            const response = await fetch("https://api.giphy.com/v1/gifs/search?q=black+and+white&api_key=uvn9u78oYla8YfLOBPBplJ6LbqCuQ8Bw&limit=1");
+            if (!response.ok)
+                throw new Error("Something wrong with the connectcion");
+            const data = await response.json();
+            const result = data.data[0];
+            console.log(result);
+            setImg(result);
+            setLoading(false);
+        } catch (error) {
+            console.log("Catch error: " + error);
+            setLoading(false);
+        }
+    }
     useEffect(() => {
-        fetch("https://api.giphy.com/v1/gifs/search?q=black+and+white&api_key=F25YrJWTElHcsQtfjKGE7LejK8EpNvYP&limit=1")
-            .then(response => response.json())
-            .then(data => {
-                () => setImg(data.data[0]);
-            })
-            .catch(error => console.log(error));
-    }, [img])
+        fetchingImage();
+    }, [])
     return (
         <div className={style.card}>
-            <img src={img.url} alt={img.alt_text} />
+            {!loading && (
+                <>
+                    <img src={img.images.original.url} alt={img.alt_text} />
+                    <p>{img.user.display_name}</p>
+                </>
+            )}
         </div>
     )
 }
