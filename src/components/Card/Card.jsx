e3c6664f9658d8f491ba92bf361510a838bfe391
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-06-28 19:12:36 +0300

    Changed the logic of fetching, but now it works so that it gets several different objects from API

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "react";
 import style from "./Card.module.scss";
 
-export default function Card({ids, setIds}) {
-    const [img, setImg] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const fetchingImage = async () => {
-        try {
-            const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=F25YrJWTElHcsQtfjKGE7LejK8EpNvYP&s=%22black%20and%20white%22");
-            if (!response.ok)
-                throw new Error("Something wrong with the connectcion");
-            const data = await response.json();
-            const result = data.data;
-            console.log(result);
-            setImg(result);
-            setLoading(false);
-        } catch (error) {
-            console.log("Catch error: " + error);
-            setLoading(false);
-        }
-    }
-    useEffect(() => {
-        fetchingImage();
-    }, [])
+export default function Card({ img }) {
+    console.log(img);
     return (
         <div className={style.card}>
-            {(!loading && img !== null) && (
-                <>
-                    <img src={img.images['downsized'].url} alt={img.alt_text} />
-                    <p>{img.title}</p>
-                </>
-            )}
+            <img src={img.images["downsized_large"].url} alt={img.title} />
+            <p>{img.title}</p>
         </div>
     )
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,33 @@
 import style from "./Cards.module.scss";
 import Card from "../Card/Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Cards() {
-    const [ids, setIds] = useState([]);
+    const numOfImages = 3;
+    const data = useData(numOfImages);
     return (
         <div className={style.cardsDiv}>
-            <Card ids={ids} setTitles={setIds} />
+            {data.map((el) => <Card img={el.data} key={el.data.id} />)}
         </div>
     )
+}
+
+function useData(numOfImages) {
+    const [data, setData] = useState([]);
+    const url = "https://api.giphy.com/v1/gifs/random?api_key=K1AltFTsgxUc2CpV2SQp8F0myqKm5k6i&tag=%22black%20and%20white%22&rating=g";
+    useEffect(() => {
+        let ignore = false;
+        async function getData() {
+            for (let i = 0; i < numOfImages; i++) {
+                const response = await fetch(url);
+                const fetchedData = await response.json();
+                !ignore && setData(prevState => {
+                    return [...prevState, fetchedData]
+                });
+            }
+        }
+        getData();
+        return () => ignore = true;
+    }, []);
+    return data;
 }
