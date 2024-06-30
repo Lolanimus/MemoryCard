64da0faae2439b0269140d40ff39caf200d69f0a
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-06-30 17:23:01 +0300

    Now, whenvere a user clicks on one of the images, page re-renders in the way that it shuffles the image array and displays images in the different order

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import style from "./Card.module.scss";
 
-export default function Card({ img }) {
-    console.log(img);
+export default function Card({ img, handleClick }) {
+    console.log();
     return (
-        <div className={style.card}>
+        <div className={style.card} onClick={handleClick}>
             <img src={img.images["downsized_large"].url} alt={img.title} />
             <p>{img.title}</p>
         </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,18 +3,46 @@ import Card from "../Card/Card";
 import { useState, useEffect } from "react";
 
 export default function Cards() {
-    const numOfImages = 12;
-    const data = useData(numOfImages);
+    const numOfImages = 3;
+    const [data, setData] = useState([]);
+    useData(numOfImages, data, setData); // pleaeeeaeadadaese use useMemo[] here i beg u
+    const handleClick = () => {
+        const newData = data.slice(); // slice() is very important here
+        // ---------------
+        // if it was const newData = data; then it means that we're just referencing the value of data,
+        // so when we use it in setData() it perceives that newData as the reference to the old data array.
+        // but if we use slice() on data, we're essentially creating a new array, a copy of data,
+        // so it will work for us to use this newData array in setData() now.
+        // ---------------
+        shuffle(newData);
+        setData(newData);
+        console.log(data);
+    }
     return (
         <div className={style.cardsDiv}>
-            {data.map((el) => <Card img={el.data} key={el.data.id} />)}
+            {data.map((el) => <Card img={el.data} handleClick={() => handleClick()} key={el.data.id} />)}
         </div>
     )
 }
 
-function useData(numOfImages) {
-    const [data, setData] = useState([]);
-    const url = "https://api.giphy.com/v1/gifs/random?api_key=K1AltFTsgxUc2CpV2SQp8F0myqKm5k6i&tag=%22black%20and%20white%22&rating=g";
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function useData(numOfImages, data, setData) {
+    const url = "https://api.giphy.com/v1/gifs/random?api_key=9QkaECus9YzMCOoy8n3SeYFzh6QsmIKF&tag=%22black%20and%20white%22&rating=g";
     useEffect(() => {
         let ignore = false;
         async function getData() {
@@ -29,5 +57,4 @@ function useData(numOfImages) {
         getData();
         return () => ignore = true;
     }, []);
-    return data;
 }
