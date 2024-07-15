d8b2eca84e40a2be391ae8021f73941d2d5f73e9
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-07-15 21:38:43 +0300

    Migrated GameInfo.tsx and GameDesc.tsx

--- src/components/GameInfo/GameInfo.tsx
+++ src/components/GameInfo/GameInfo.tsx
@@ -3,12 +3,12 @@ import style from "./GameInfo.module.scss";
 import { UserContext } from "../../App";
 
 export default function GameInfo() {
-    const bestScoreState = useContext(UserContext).bestScoreState;
-    const scoreState = useContext(UserContext).scoreState;
+    const bestScoreState = useContext(UserContext)!.bestScoreState;
+    const scoreState = useContext(UserContext)!.scoreState;
     return (
         <div className={style.info}>
-            <p id="score">Score: {scoreState[0]}</p>
-            <p id="bestScore">Best score: {bestScoreState[0]}</p>
+            <p id="score">Score: {scoreState[0] as number}</p>
+            <p id="bestScore">Best score: {bestScoreState[0] as number}</p>
         </div>
     )
 }
